9bde8fec6649999b6368d10f52b998c69e093b14
Pibotics3814 <pibotics3814@gmail.com>
2024-02-06 18:09:52 -0500

    hfghj hg

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -48,7 +48,7 @@ public class RobotContainer {
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   XboxController driveStick = new XboxController(2);
-  XboxController controlStick = new XboxController(2);
+  XboxController controlStick = new XboxController(1);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
